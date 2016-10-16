cbac9d2aea8bb8515fb16c4cea8beaa484bf6d45
ryan niemes <ryan.niemes@gmail.com>
2016-10-15 22:09:01 -0700

    updated to use requests.Session

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-from grrsdk import GRRClient
+import grrsdk
 
-g = GRRClient()
+g = grrsdk.GRRClient()
 g.print_client_info('10.12.52.32')
+g.iterate_clients()
+#g.execute_python_hack('get_mbambr.py', '10.12.52.32')
--- grrsdk.py
+++ grrsdk.py
@@ -1,4 +1,3 @@
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 import argparse
 import base64
 import ConfigParser
@@ -6,14 +5,15 @@ import json
 import logging
 import logging.config
 import os
+import sys
 import requests
 import time
 
 config = ConfigParser.ConfigParser()
 config.read('grrsdk.ini')
-baseurl = config.get('grrsdk','baseurl')
-username = config.get('grrsdk','username')
-password = config.get('grrsdk','password')
+baseurl = config.get('grrsdk', 'baseurl')
+username = config.get('grrsdk', 'username')
+password = config.get('grrsdk', 'password')
 
 logging.config.fileConfig('logging.conf')
 
@@ -28,16 +28,20 @@ def read_file_contents(path):
 
 class GRRClient:
     logger.debug('in class GRRClient')
-    token = None
-    session = requests.Session()
-    jdata = {}
-    
+
     def __init__(self):
+        self.data = {}
+        # self.token = None
         self.token = self.load_token()
-        self.end = int(time.time() * 1000)   # time we are running this script
+        self.start = int(time.time() * 1000)
+        self.session = requests.Session()
+        self.session.auth = (username, password)
 
         if self.token:
-            self.session.headers.update({'X-csrftoken': self.token})
+            #self.session.headers.update({'X-csrftoken': self.token})
+            self.session.cookies.update({'csrftoken': self.token})
+            #self.session.cookies.set({'csrftoken': self.token,
+            #                          'domain': 'grr.illumina.com'})
         else:
             # if no token, do login
             if not self.token:
@@ -47,106 +51,120 @@ class GRRClient:
                     sys.exit('Username-password invalid')
 
        # if self.start == None:
-       #     self.start = 1463441314000
-
-    def login(self):
-        logger.debug('method=login')
-        base64string = base64.encodestring('%s:%s' % (username,password)).replace('\n', '')
-        logger.debug('method=login, base64string=%s' % (base64string))
-        authheader = "Basic %s" % base64string
-        self.index_response = requests.get(baseurl, auth=(username,password))
-        #self.session = session.get(baseurl, auth=(username,password))
-        csrf_token = self.index_response.cookies.get('csrftoken')
+       #     self.start =
         self.headers = {
-        	"Authorization": authheader,
-        	"x-csrftoken": csrf_token,
-        	"x-requested-with": "XMLHttpRequest"
+#            "Authorization": authheader,
+            "x-csrftoken": self.token,
+            "x-requested-with": "XMLHttpRequest"
         }
 
-    def is_installed(self,ip):
+    def login(self):
+        print 'running login'
+        # self.index_response = requests.get(baseurl, auth=(username,password))
+        # self.session = requests.Session()
+        # self.session.auth = (username, password)
+        self.session.get(baseurl)
+        print self.session.cookies
+
+        xsrf_token = self.session.cookies.get('csrftoken')
+        self.session.headers.update({'x-csrftoken': xsrf_token,
+                                     'x-requested-with': 'XMLHttpRequest'})
+
+        print xsrf_token
+        with open('token', 'w') as f:
+            f.write(xsrf_token)
+
+        self.headers = {
+           # "Authorization": authheader,
+            "x-csrftoken": xsrf_token,
+            "x-requested-with": "XMLHttpRequest"
+        }
+
+    def is_installed(self, ip):
         """
         is_installed(ip) takes the argument of an IP address and returns 1 if grr is installed on that host, 0 otherwise
         """
         logger.debug('method=is_installed')
-        self.grr_query_ip(ip)
-    	if self.jdata['items']:
+        self.query_ip(ip)
+        print self.session.headers
+
+        if self.data['items']:
             logger.debug('grr is already installed on %s' % (ip))
             return 1
-    	else:
+        else:
             logger.debug('grr is not installed on %s' % (ip))
             return 0
 
     def load_token(self):
         return read_file_contents('token')
 
-    def grr_query_ip(self,ip):
-        logger.debug('method=grr_query_ip, args=%s' % (ip))
-        req = ('%s/api/clients?query=%s' % (baseurl, ip))
+    def query_ip(self, ip):
+        logger.debug('method=query_ip, args=%s' % ip)
+        req = ('%s/api/clients?query=%s&strip_type_info=1' % (baseurl, ip))
         # the text[5:] is to remove the first few characters that are in the JSON response for XSSI protection
-        r = requests.get(req, auth=(username, password)).text[5:]
-        self.jdata = json.loads(r)
+        r = self.session.get(req, cookies=self.session.cookies).text[5:]
+        self.data = json.loads(r)
         return 0
 
-    def grr_get_all_clients(self):
+    def get_all_clients(self):
         """
-        grr_get_all_clients has no returns, simply loading up the json object of self.jdata with the results
+        grr_get_all_clients has no returns, simply loading up the json object of self.data with the results
         """
-        req = ('%s/api/clients' % (baseurl))
+        req = ('%s/api/clients&strip_type_info=1' % (baseurl))
         # the text[5:] is to remove the first few characters that are in the JSON response for XSSI protection
-        r = requests.get(req, auth=(username,password)).text[5:]
-        self.jdata = json.loads(r)
+        r = self.session.get(req, cookies=self.session.cookies)
+        print r
+        r.raise_for_status()
+
+        self.data = json.loads(r)
+        print json.dumps(r, indent=4, sort_keys=True)
 
     def iterate_clients(self):
         """
         iterate_clients prints the fqdn and urn of every client in grr
         """
-        self.grr_get_all_clients()
-        for host in self.jdata['items']:
+        self.get_all_clients()
+        for host in self.data['items']:
             try:
-                print '%s,%s' % (host['value']['os_info']['value']['fqdn']['value'], host['value']['urn']['value'][6:])
+                print '%s,%s' % (host['os_info']['fqdn'], host['urn'][6:])
             except KeyError as e:
-                print "unknown,%s" % (host['value']['urn']['value'][6:])
+                print "unknown,%s" % (host['urn'][6:])
 
-    def print_client_info(self,ip):
+    def print_client_info(self, ip):
         """
         this method takes an IP address and prints basic information about the host
         """
         if self.is_installed(ip):
-        #    print json.dumps(self.jdata, indent=4, sort_keys=True)
             try:
-                print '%s,%s' % (self.jdata['items'][0]['value']['os_info']['value']['fqdn']['value'],
-                                 self.jdata['items'][0]['value']['urn']['value'][6:])
+                print '%s,%s' % (self.data['items'][0]['os_info']['fqdn'],
+                                 self.data['items'][0]['urn'][6:])
             except KeyError as e:
-                print "unknown,%s" % (self.jdata['items'][0]['value']['urn']['value'][6:])
+                print 'Error in method print_client_info: %s' % e, ': %s'
         else:
             print 'client unknown: %s' % (ip)
 
-#def install_grr(ip):
-#    #hclogger.info(message="inside method install_grr(): installing grr on %s" % (ip))
-#    command = "%s \\\\%s -c -f -s %s" % (myconf.take('executable_locations.psexec'), ip, myconf.take('executable_locations.grr_64bit'))
-#    #hclogger.info(message="full command line %s" % (command))
-#    try:
-#    #    hclogger.info(message='install_grr running')
-#        subprocess.call(command)
-#        #hclogger.info(message='install_grr proc return code' % (proc))
-#    except:
-#        print 'exception in install_grr'
-#    #    hclogger.info(message="exception inside install_grr %s" % (sys.exc_info()[0]))
-#
-#        #stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#    return proc
-
-
-    def get_urn_from_ip(self,ip):
+    def get_urn_from_ip(self, ip):
+        """
+        this method returns the client URN from a given IP address
+        :param ip:
+        :return:
+        """
         logger.debug('inside method get_urn_from_ip')
         req = ('%s/api/clients?query=%s' % (baseurl, ip))
         # the text[5:] is to remove the first few characters that are in the JSON response for XSSI protection
-        r = requests.get(req, auth=(username, password)).text[5:]
+        # r = requests.get(req, auth=(username, password)).text[5:]
+        r = self.session.get(req).text[5:]
         jdata = json.loads(r)
         urn = jdata['items'][0]['value']['urn']['value'][6:]
         return urn
 
-    def execute_python_hack(self,hack_name,ip):
+    def execute_python_hack(self, hack_name, ip, notify=0):
+        """
+        this method runs a python hack on the IP address passed to the method
+        :param hack_name:
+        :param ip:
+        :return: success or an error message
+        """
         python_hack = {
             "flow": {
                 "args": {
@@ -154,21 +172,20 @@ class GRRClient:
                 },
                 "name": "ExecutePythonHack",
                 "runner_args": {
-                    "notify_to_user": 0,
+                    "notify_to_user": notify,
                     "output_plugins": [],
                     "priority": "HIGH_PRIORITY"
                 }
             }
         }
-        print python_hack
         urn = self.get_urn_from_ip(ip)
-        print urn
-        req = ('%s/api/clients/%s/flows' % (baseurl, urn))
-        r = requests.post(req, auth=(username, password)).text[5:]
-        logger.debug('full req %s' % (req))
-        r = requests.post(req, data=json.dumps(python_hack), headers=self.headers, cookies=self.index_response.cookies)    
+        req = ('%s/api/clients/%s/flows?strip_type_info=1' % (baseurl, urn))
+        result = self.session.post(req, data=json.dumps(python_hack), headers=self.headers, cookies=self.session.cookies).text[5:]
+        #print result
+        print json.dumps(result, indent=4, sort_keys=True)
+        #return result
 
-    def run_flow(self,flow_name,urn):
+    def run_flow(self, flow_name, urn):
         flow = {
             "flow": {
                 "name": flow_name,
