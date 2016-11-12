256c009a8a6a57e35cd2dc7806c74b8e04906ac9
ryan niemes <ryan.niemes@gmail.com>
2016-11-12 06:28:54 -0800

    updated to use requests.Session

--- example.py
+++ example.py
@@ -2,5 +2,19 @@ import grrsdk
 
 g = grrsdk.GRRClient()
 g.print_client_info('10.12.52.32')
+
 #g.iterate_clients()
 #g.execute_python_hack('get_mbambr.py', '10.12.52.32')
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def foo():
+   data = json.loads(request.data)
+   print "New commit by: {}".format(data['commits'][0]['author']['name'])
+   return "OK"
+
+if __name__ == '__main__':
+   app.run()
